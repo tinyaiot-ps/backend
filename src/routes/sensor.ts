1e69565a3aa8649fb9c50256c821668f6b418035
Ahsan Farooq <ahsanfarooq3423@gmail.com>
2024-06-05 09:52:23 +0200

    Sensors module finalize

--- src/routes/sensor.ts
+++ src/routes/sensor.ts
@@ -10,12 +10,12 @@ import {
 const router = express.Router();
 
 // GET ALL sensors
-router.get('/sensors', authenticateToken, getAllSensors); // Controller logic for getting all sensors goes here
+router.get('/', authenticateToken, getAllSensors); // Controller logic for getting all sensors goes here
 
 // GET sensor by id
-router.get('/sensors/:id', authenticateToken, getSensorById);
+router.get('/:id', authenticateToken, getSensorById);
 
 // POST sensor
-router.post('/sensors', authenticateToken, postSensor);
+router.post('/', authenticateToken, postSensor);
 
 export default router;
