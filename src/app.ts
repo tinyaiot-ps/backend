0064a50dafbf21c692b4656831318328238342ce
Ahsan Farooq <ahsanfarooq3423@gmail.com>
2024-07-09 15:16:19 +0200

    Misc. changes

--- src/app.ts
+++ src/app.ts
@@ -19,30 +19,9 @@ const PORT = process.env.PORT || 5001;
 
 dotenv.config();
 
+// import './mqttClient';
 // Middleware
 app.use(bodyParser.json());
-
-const client = mqtt.connect('mqtt://eu1.cloud.thethings.network:1883', {
-  username: 'tinyaiot-project-seminar@ttn',
-  password: process.env.MQTT_CLIENT_KEY,
-});
-
-client.on('connect', () => {
-  console.log('Connected to MQTT broker');
-  const topic = 'v3/tinyaiot-project-seminar@ttn/devices/trash-bin-01/up';
-  client.subscribe(topic, () => {
-    console.log(`Subscribe to topic '${topic}'`);
-  });
-});
-
-client.on('message', (topic: any, message: any) => {
-  console.log('EVENT RECIEVED with TOPIC', topic);
-  console.log('Received message:', message.toString());
-});
-
-client.on('error', (error: any) => {
-  console.error('MQTT connection error:', error);
-});
 
 const corsOptions = {
   origin: [`http://localhost:${PORT}`],
